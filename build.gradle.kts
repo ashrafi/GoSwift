467830c3032b0d459e9981e455c1a4b8b71fcf6c
Zoewave Developer <developer@zoewave.com>
2020-08-28 07:24:41 -0700

    update to Compose alpha 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("com.android.application") version "4.2.0-alpha07" apply false
-    kotlin("android") version "1.4-M3" apply false
+    id("com.android.application") version "4.2.0-alpha08" apply false
+    kotlin("android") version "1.4.0" apply false
 }
 
 allprojects {
     repositories {
         mavenCentral()
-        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap/")
+        //maven(url = "https://dl.bintray.com/kotlin/kotlin-eap/")
         jcenter()
         google()
     }
